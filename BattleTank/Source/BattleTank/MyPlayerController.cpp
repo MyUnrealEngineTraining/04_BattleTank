4ee653370c969c9f7f58b2df1b277e5f90b9475b
Yurii <predbannikov.yurii@gmail.com>
2021-01-09 00:47:07 +0300

    124

--- BattleTank/Source/BattleTank/MyPlayerController.cpp
+++ BattleTank/Source/BattleTank/MyPlayerController.cpp
@@ -19,12 +19,67 @@ void AMyPlayerController::BeginPlay()
 void AMyPlayerController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
 }
 
 void AMyPlayerController::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return; }
+	
+	FVector HitLocation = FVector(0);
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
 
+	}
+
+}
+
+bool AMyPlayerController::GetSightRayHitLocation(FVector &HitLocation) const
+{
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection)) {
+		UE_LOG(LogTemp, Warning, TEXT("Deproject: %s"), *LookDirection.ToString()); 
+		GetLookVectorHitLocation(LookDirection, HitLocation);
+	}
+//	DrawDebugLine(
+//		GetWorld(),
+//		GetOwner()->GetActorLocation(),
+//		WorldDirection,
+//		FColor(255, 0, 0),
+//		false,
+//		0.0f,
+//		0.0f,
+//		10.0f
+//	);
+	return true;
+}
+
+bool AMyPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const
+{
+	FHitResult HitResult;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + (LookDirection * LineTraceRange);
+	if (GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		StartLocation,
+		EndLocation,
+		ECollisionChannel::ECC_Visibility))
+	{
+		HitLocation = HitResult.Location;
+		return true;
+	}
+	return false;
+}
+
+bool AMyPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector WorldDirection;
+	FVector CameraWorldLocation;
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, CameraWorldLocation, LookDirection);
 }
 
 
